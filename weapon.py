675c4c054fce69183756f87587a6d08897192f25
Alex Kubiesa <akubiesa@btinternet.com>
2018-11-17 21:31:44 +0000

    Make the weapons random colours for now.

--- weapon.py
+++ weapon.py
@@ -1,7 +1,9 @@
 import pygame
+import random
 
 
 class Weapon:
 
     def __init__(self):
         self.icon = pygame.Surface((20, 20))
+        self.icon.fill((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
